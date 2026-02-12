2e5b923c2129ffdbac3570063fde9ce96f3bfb0b
DalilahH1 <106951589+MajesticL@users.noreply.github.com>
2026-02-12 05:47:34 -0500

    commented out components until they have been created

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
 import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
-import About from "./components/About";
-import Projects from "./components/Projects";
-import Contact from "./components/Contact";
-import Footer from "./components/footer";
+// import Hero from "./components/Hero";
+// import About from "./components/About";
+// import Projects from "./components/Projects";
+// import Contact from "./components/Contact";
+// import Footer from "./components/footer";
 
 function App() {
   return (
     <>
       <Navbar />
-      <Hero />
+      {/* <Hero />
       <About />
       <Projects />
       <Contact />
-      <Footer />
+      <Footer /> */}
     </>
   );
 }
