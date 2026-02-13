8183bbb0405ada1446a0aa550401fbcd2d0fb000
DalilahH1 <106951589+MajesticL@users.noreply.github.com>
2026-02-13 00:22:28 -0500

    fixed some spacing/Added hero-content class

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,15 +5,17 @@ function Hero() {
     return (
         <div id="home">
             <section className="hero">
-                <h1>We craft majestic web experiences</h1>
-                <span>Where Vision Meets Digital Excellence</span>
-                <p>Exceptional design is within reach. We create custom websites that bring your brand's unique story to life and drive meaningful business growth.</p>
-                <div className="hero-buttons">
-                    <a href="#portfolio">View Our Work</a>
-                    <a href="#contact">Start Your Project</a>
+                <div className="hero-content">
+                    <h1>We craft majestic web experiences</h1>
+                    <span>Where Vision Meets Digital Excellence</span>
+                    <p>Exceptional design is within reach. We create custom websites that bring your brand's unique story to life and drive meaningful business growth.</p>
+                    <div className="hero-buttons">
+                        <a href="#portfolio">View Our Work</a>
+                        <a href="#contact">Start Your Project</a>
+                    </div>
+                    <img src="/images/frame.jpg" alt="Hands holding a crown" />
                 </div>
             </section>
-            <img src="/images/frame.jpg" alt="Hands holding a crown" />
         </div>
     );
 }
