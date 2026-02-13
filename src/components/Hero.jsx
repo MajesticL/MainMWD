e7113476faf97d3d1adac0742cf7aea7834e67bc
DalilahH1 <106951589+MajesticL@users.noreply.github.com>
2026-02-13 00:58:04 -0500

    replaced hero-content with global container css

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,7 +5,7 @@ function Hero() {
     return (
         <div id="home">
             <section className="hero">
-                <div className="hero-content">
+                <div className="container">
                     <h1>We craft majestic web experiences</h1>
                     <span>Where Vision Meets Digital Excellence</span>
                     <p>Exceptional design is within reach. We create custom websites that bring your brand's unique story to life and drive meaningful business growth.</p>
