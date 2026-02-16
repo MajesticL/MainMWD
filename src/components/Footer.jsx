b6d53120bbea9f68cc14ed195fe4b5cf90076d42
DalilahH1 <106951589+MajesticL@users.noreply.github.com>
2026-02-15 23:48:14 -0500

    Removed dublicated import

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,5 @@
-import React from 'react'import React from 'react'
-import '../styles/Services.css'
+import React from 'react';
+import '../styles/Services.css';
 
 function Footer() {
     return (
