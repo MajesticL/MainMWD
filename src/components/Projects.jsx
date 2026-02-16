907023bf62a7a29ec9ed222105425536ab492b24
DalilahH1 <106951589+MajesticL@users.noreply.github.com>
2026-02-15 23:46:44 -0500

    Added Projects content including projects list, Imports ProjectCard

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,2 +1,61 @@
-import React from 'react'
-import ProjectCard from './ProjectCard'
+import React from 'react';
+import ProjectCard from './ProjectCard';
+import '../styles/Projects.css';
+
+
+function Projects() {
+    const projectList = [
+        {
+            id: 1,
+            title: "Golden Crumb Bakery",
+            description: "Simple 1-page bakery template with a menu.",
+            image: "../images/bakery.png",
+            link: "https://goldencrumb.netlify.app/",
+            github: ""
+        },
+        {
+            id: 2,
+            title: "Todo List",
+            description: "Functional Todo List with a style toggle for dark and light mode. Keeps tasks as page is refreshed.",
+            image: "../images/todo.png",
+            link: "https://testingtodos.netlify.app/",
+            github: ""
+        },
+        {
+            id: 3,
+            title: "Paw and Bubble",
+            description: "Cute dog groomer template with a services, gallery,and testimonial section. Perfect foundation for your fuzzy friends.",
+            image: "../images/paws.png",
+            link: "https://pawandbubble.netlify.app/",
+            github: ""
+        }
+        // {
+        //     id: 1,
+        //     title: ,
+        //     description: ,
+        //     image: "",
+        //     link: "",
+        //     github: ""
+        // },
+        
+    ]
+    return (
+        <section id="portfolio" className="projects-section">
+      <h2>My Recent Work</h2>
+      
+      <div className="projects-grid">
+        {projectList.map((project) => (
+          <ProjectCard 
+            key={project.id}
+            title={project.title}
+            description={project.description}
+            image={project.image}
+            link={project.link}
+          />
+        ))}
+      </div>
+    </section>
+    );
+}
+
+export default Projects;
