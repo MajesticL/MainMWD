04f232ce45772845bca64453f7da3cf0dd8f0821
DalilahH1 <106951589+MajesticL@users.noreply.github.com>
2026-02-15 23:42:49 -0500

    Imported page components (Services, About, Projects)

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
-// import About from "./components/About";
-// import Projects from "./components/Projects";
+import Services from "./components/Services";
+import About from "./components/About";
+import Projects from "./components/Projects";
 // import Contact from "./components/Contact";
 // import Footer from "./components/Footer";
 
@@ -10,7 +11,10 @@ function App() {
     <>
       <Navbar />
       <Hero />
-      {/* <About />
+      <Services />
+      <About />
+      <Projects />
+      {/* 
       <Projects />
       <Contact />
       <Footer /> */}
