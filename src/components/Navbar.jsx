abcf49d0127bcc517397fa38629debfa9292e329
DalilahH1 <106951589+MajesticL@users.noreply.github.com>
2026-03-19 15:00:11 -0400

    Add toggle menu function through inline js, remove white space

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,49 +1,63 @@
-import React, { useState } from 'react';
-import '../styles/Navbar.css'
+import React, { useState } from "react";
+import "../styles/Navbar.css";
 
 function Navbar() {
-    const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-    const toggleMenu = () => {
-        setIsMenuOpen(!isMenuOpen);
-    };
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
 
-    return (
-        <header>
-            <nav id="nav">
-                <div className="logo">
-                    <a href="https://www.majestywebmagic.com/">
-                        <span>Majesty Web Design</span>
-                        <img src="/images/resizedLogo.png" alt="Majesty Web Design LLC Logo" />
-                    </a>
-                </div>
-                <div className='link-column'>
+  return (
+    <header>
+      <nav id="nav">
+        <div className="logo">
+          <a href="https://www.majestywebmagic.com/">
+            <span>Majesty Web Design</span>
+            {/* <img
+              src="/images/resizedLogo.png"
+              alt="Majesty Web Design LLC Logo"
+            /> */}
+          </a>
+        </div>
 
-                    <ul className={`nav-links ${isMenuOpen ? 'active' : ''}`} id="navLinks">
-                        <li><a href="#about">About Me</a></li>
-                        {/* <li><a href="#home">Home</a></li> */}
-                        <li><a href="#services">Services</a></li>
-                        
-                        <li><a href="#portfolio">Portfolio</a></li>
-                    </ul>
-                
-                    <a href="#contact" className="cta-button">Get In Contact</a>
-                </div>
-                
-                
-                <button 
-                    className={`hamburger ${isMenuOpen ? 'active' : ''}`} 
-                    id="hamburger" 
-                    onClick={toggleMenu} 
-                    title="nav-toggle"
-                >
-                    <span></span>
-                    <span></span>
-                    <span></span>
-                </button>
-            </nav>
-        </header>
-    );
+        <div className={`link-column ${isMenuOpen ? "active" : ""}`}>
+          <ul className="nav-links" id="navLinks">
+            <li>
+              <a href="#about" onClick={toggleMenu}>
+                About Me
+              </a>
+            </li>
+            <li>
+              <a href="#services" onClick={toggleMenu}>
+                Services
+              </a>
+            </li>
+            <li>
+              <a href="#portfolio" onClick={toggleMenu}>
+                Portfolio
+              </a>
+            </li>
+          </ul>
+
+          <a href="#contact" className="cta-button" onClick={toggleMenu}>
+            Get In Contact
+          </a>
+        </div>
+
+        <button
+          className={`hamburger ${isMenuOpen ? "active" : ""}`}
+          id="hamburger"
+          onClick={toggleMenu}
+          title="nav-toggle"
+        >
+          <span></span>
+          <span></span>
+          <span></span>
+        </button>
+      </nav>
+    </header>
+  );
 }
 
 export default Navbar;
